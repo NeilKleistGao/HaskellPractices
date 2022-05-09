96b0356d74f37419950967d4ea98c7199ab7f83e
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-05-09 16:49:19 +0800

    Finish types

--- types.hs
+++ types.hs
@@ -6,4 +6,19 @@ type RGB = (Float, Float, Float)
 a :: Int
 a = 42
 
-main = putStrLn("rua")
+isTwo :: Int -> Bool
+isTwo(x) = if x == 2 then True else False
+
+check :: Int -> Int
+check(n) = case n of
+    1 -> 114
+    2 -> 514
+    3 -> 1919
+    4 -> 810
+    _ -> error "?" 
+
+f :: Int -> Int
+f n | n > 1 = f(n - 1) * n
+    | otherwise = 1
+
+main = if (f 4) == 24 then putStrLn "rua" else putStrLn "GG"
